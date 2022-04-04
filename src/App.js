2d9f93ca740e99bab98c949171dcf5c4c9787c18
Manmeet <manmeet.singh@knoldus.com>
2022-04-04 23:50:15 +0530

    Pizzeria Food Order App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router} from
+ 'react-router-dom';
+import Feature from './components/Feature';
+import { GlobalStyle } from "./components/globalStyle";
+import Hero from "./components/Hero";
+import Products from './components/Products';
+import { productData, productDataTwo } from './components/Products/data';
+import Footer from './components/Footer';
 
-function App() {
+ function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <GlobalStyle />
+      <Hero />
+      <Products heading='Choose your Favourite' data={productData}/>
+      <Feature />
+      <Products heading='Choose your Favourite Sweets' data={productDataTwo}/>
+      <Footer />
+    </Router>
   );
 }
 
